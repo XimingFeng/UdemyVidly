0e239cf4bcfd3947c424556dbcd10ea0016a2ea0
XimingFeng <royfeng123@gmail.com>
2022-03-07 21:06:17 -0700

    Push to master for the night while hacking away the exercise of section 6

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.DTOs;
+using AutoMapper;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesContro                                             ller()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public IEnumerable<MovieDTO> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDTO>);
+        }
+    }
+}
---
+++ Vidly/DTOs/MovieDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.DTOs
+{
+    public class MovieDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int GenreID { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
